806bbbfc2a28f419a4bb5c8fcd348f4b4f2aef16
Thomas Grossen <git@grossen.me>
2017-12-27 16:39:53 +0100

    Initial Commit

---
+++ PreferencesTutorial/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PreferencesTutorial
+//
+//  Created by Thomas on 12.12.17.
+//  Copyright © 2017 Thomas Grossen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
